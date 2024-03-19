a791e22ab7d14f6881160f9234150aac985ec365
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2024-03-19 17:36:24 +0530

    second commit

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     compress: true,
     historyApiFallback: true,
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'http://localhost:8000',
     },
   },
   module: {
